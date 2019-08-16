85d3a2205caeccf445d1a6826953d5be9d0297fe
amy860702 <amy860702@gmail.com>
2019-08-16 22:46:56 +0800

    Add Schedule_card model

---
+++ app/Schedule_card.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule_card extends Model
+{
+    protected $fillable = [
+        'title', 'describe', 'article_url', 'article_content', 'image_url', 'owner_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    public function schedule_card_mapping()
+    {
+        return $this->hasMany(Schedules_cards_mapping::class, 'id');
+    }
+}
