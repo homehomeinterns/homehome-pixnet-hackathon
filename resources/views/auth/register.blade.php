091574991f712ebe26760ad27d5b2246ec01cb29
amy860702 <amy860702@gmail.com>
2019-08-16 22:50:27 +0800

    Modified: Register 新增欄位 nick_name, gender, birth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,6 +25,48 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('nick_name') ? ' has-error' : '' }}">
+                            <label for="nick_name" class="col-md-4 control-label">Nick name</label>
+
+                            <div class="col-md-6">
+                                <input id="nick_name" type="text" class="form-control" name="nick_name" value="{{ old('nick_name') }}" required autofocus>
+
+                                @if ($errors->has('nick_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('nick_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
+                            <label for="gender" class="col-md-4 control-label">Gender</label>
+
+                            <div class="col-md-6">
+                                <input id="gender" type="text" class="form-control" name="gender" value="{{ old('gender') }}" required autofocus>
+
+                                @if ($errors->has('gender'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('gender') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('birth') ? ' has-error' : '' }}">
+                            <label for="birth" class="col-md-4 control-label">Birth</label>
+
+                            <div class="col-md-6">
+                                <input id="birth" type="text" class="form-control" name="birth" value="{{ old('birth') }}" required autofocus>
+
+                                @if ($errors->has('birth'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('birth') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
