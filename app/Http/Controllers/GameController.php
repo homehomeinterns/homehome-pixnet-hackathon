5cc3fd205a478d37416b770d25aedb97fbdb09cf
homehomeinterns@gmail.com <jane8@instance-1.us-east1-b.c.awesome-delight-248313.internal>
2019-08-17 15:21:43 +0000

    edit travel_game & option model for ques api

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,91 +10,92 @@ use App\Keyword;
 use App\keyword_spot_mappings;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class GameController extends Controller
 {
     public function ques()
     {
-	$ques_1 = DB::table('travel_games')->where('id', '1')->first();
-	$ques_2 = DB::table('travel_games')->where('id', '2')->first();
-	$ques_3 = DB::table('travel_games')->where('id', '3')->first();
-	$ques_4 = DB::table('travel_games')->where('id', '4')->first();
-	$ques_5 = DB::table('travel_games')->where('id', '5')->first();
-	$ques_6 = DB::table('travel_games')->where('id', '6')->first();
-	$ques_7 = DB::table('travel_games')->where('id', '7')->first();
-	$ques_8 = DB::table('travel_games')->where('id', '8')->first();
+	$ques_1 = Travel_game::where('id', '1')->first();
+	$ques_2 = Travel_game::where('id', '2')->first();
+	$ques_3 = Travel_game::where('id', '3')->first();
+	$ques_4 = Travel_game::where('id', '4')->first();
+	$ques_5 = Travel_game::where('id', '5')->first();
+	$ques_6 = Travel_game::where('id', '6')->first();
+	$ques_7 = Travel_game::where('id', '7')->first();
+	$ques_8 = Travel_game::where('id', '8')->first();
 	
 	$question = array (
 		array (
 			"question" => $ques_1->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_1->option_A)->first()->option_content,
+				Option::where('id', $ques_1->option_B)->first()->option_content,
+				Option::where('id', $ques_1->option_C)->first()->option_content,
+				Option::where('id', $ques_1->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_2->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_2->option_A)->first()->option_content,
+				Option::where('id', $ques_2->option_B)->first()->option_content,
+				Option::where('id', $ques_2->option_C)->first()->option_content,
+				Option::where('id', $ques_2->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_3->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_3->option_A)->first()->option_content,
+				Option::where('id', $ques_3->option_B)->first()->option_content,
+				Option::where('id', $ques_3->option_C)->first()->option_content,
+				Option::where('id', $ques_3->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_4->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_4->option_A)->first()->option_content,
+				Option::where('id', $ques_4->option_B)->first()->option_content,
+				Option::where('id', $ques_4->option_C)->first()->option_content,
+				Option::where('id', $ques_4->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_5->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_5->option_A)->first()->option_content,
+				Option::where('id', $ques_5->option_B)->first()->option_content,
+				Option::where('id', $ques_5->option_C)->first()->option_content,
+				Option::where('id', $ques_5->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_6->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_6->option_A)->first()->option_content,
+				Option::where('id', $ques_6->option_B)->first()->option_content,
+				Option::where('id', $ques_6->option_C)->first()->option_content,
+				Option::where('id', $ques_6->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_7->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_7->option_A)->first()->option_content,
+				Option::where('id', $ques_7->option_B)->first()->option_content,
+				Option::where('id', $ques_7->option_C)->first()->option_content,
+				Option::where('id', $ques_7->option_D)->first()->option_content
 			)
 		),
 		array (
 			"question" => $ques_8->question_title,
 			"answer" => array(
-				"天空漸亮的清晨",
-				"陽光普照的上午",
-				"天空漸暗的下午",
-				"夜幕低垂的夜晚"
+				Option::where('id', $ques_8->option_A)->first()->option_content,
+				Option::where('id', $ques_8->option_B)->first()->option_content,
+				Option::where('id', $ques_8->option_C)->first()->option_content,
+				Option::where('id', $ques_8->option_D)->first()->option_content
 			)
 		)
 	);
@@ -114,7 +115,11 @@ class GameController extends Controller
 
     public function test()
     {
-	$ques = DB::table('travel_games')->where('id', '1')->get();
-	return $ques;
+	//$ques = DB::table('travel_games')->where('id', '1')->first();
+	//$question = $ques->question_title;
+	
+	$ques = Travel_game::where('id', '1')->first();
+	$question_title = $ques->question_title;
+	return response()->json($question_title);
     }
 }
--- app/Travel_game.php
+++ app/Travel_game.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Travel_game extends Model
 {
-    //
+    protected $fillable = [
+	'id', 'question_title', 'option_A', 'option_B', 'option_C', 'option_D'
+    ];
 }
