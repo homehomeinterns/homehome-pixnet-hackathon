004480250f79c66887fa5bf79a2ef818f52818d9
homehomeinterns@gmail.com <jane8@instance-1.us-east1-b.c.awesome-delight-248313.internal>
2019-08-17 14:22:06 +0000

    edit ques api

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Travel_game;
+use App\Spot;
+use App\Option;
+use App\Option_keyword_mappings;
+use App\Keyword;
+use App\keyword_spot_mappings;
+
+use Illuminate\Support\Facades\Auth;
+
 class GameController extends Controller
 {
     public function ques()
     {
+	$ques_1 = DB::table('travel_games')->where('id', '1')->first();
+	$ques_2 = DB::table('travel_games')->where('id', '2')->first();
+	$ques_3 = DB::table('travel_games')->where('id', '3')->first();
+	$ques_4 = DB::table('travel_games')->where('id', '4')->first();
+	$ques_5 = DB::table('travel_games')->where('id', '5')->first();
+	$ques_6 = DB::table('travel_games')->where('id', '6')->first();
+	$ques_7 = DB::table('travel_games')->where('id', '7')->first();
+	$ques_8 = DB::table('travel_games')->where('id', '8')->first();
+	
 	$question = array (
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_1->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -17,7 +35,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_2->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -26,7 +44,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_3->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -35,7 +53,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_4->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -44,7 +62,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_5->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -53,7 +71,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_6->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -62,7 +80,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_7->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -71,7 +89,7 @@ class GameController extends Controller
 			)
 		),
 		array (
-			"question" => "睜開眼睛，你發現眼前有一扇如同《霍爾的移動城堡》的魔法之門，你覺得當你轉開門把時，門外的世界正處於？",
+			"question" => $ques_8->question_title,
 			"answer" => array(
 				"天空漸亮的清晨",
 				"陽光普照的上午",
@@ -92,5 +110,11 @@ class GameController extends Controller
     public function spot($spot)
     {
 		
+    }
+
+    public function test()
+    {
+	$ques = DB::table('travel_games')->where('id', '1')->get();
+	return $ques;
     }
 }
