9fef9324e0038896ec7c24206d13b17b3f3b7530
homehomeinterns@gmail.com <jane8@instance-1.us-east1-b.c.awesome-delight-248313.internal>
2019-08-18 10:22:45 +0000

    edit spot api

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 use App\Travel_game;
 use App\Spot;
 use App\Option;
-use App\Option_keyword_mappings;
+use App\Option_keyword_mapping;
 use App\Keyword;
-use App\keyword_spot_mappings;
+use App\Keyword_spot_mapping;
 
 class GameController extends Controller
 {
@@ -102,8 +102,67 @@ class GameController extends Controller
 	return urldecode(json_encode($question));
     }
 
-    public function spot($answer)
+    public function spot($ans)
     {
-	return $answer[1];
+	for ($i=1;$i<=92;$i++){
+		$spot[0][$i] = Spot::where('id', $i)->first()->start_point;
+		$spot[1][$i] = $i;
+	}
+
+	for ($i=0;$i<8;$i++){
+		if ($ans[$i] == "A"){
+			$option_id[$i] = ($i+1)*10+1;
+		}	
+		else if ($ans[$i] == "B"){
+			$option_id[$i] = ($i+1)*10+2;
+		}
+		else if ($ans[$i] == "C"){
+			$option_id[$i] = ($i+1)*10+3;
+		}
+		else if ($ans[$i] == "D"){
+			$option_id[$i] = ($i+1)*10+4;
+		}
+		$keyword_id[$i] = Option_keyword_mapping::where('option_id', $option_id[$i])->get();
+		$keyword_id_num[$i] = count($keyword_id[$i]);
+		for ($j=0;$j<$keyword_id_num[$i];$j++){
+			$spot_id[$i] = Keyword_spot_mapping::where('keyword_id', $keyword_id[$i][$j]->keyword_id)->get();
+			$spot_id_num[$i] = count($spot_id[$i]);
+			for ($k=0;$k<$spot_id_num[$i];$k++){
+				$id = Spot::where('id', $spot_id[$i][$k]->spot_id)->first()->id;
+				$spot[0][$id]++;
+			}
+		}
+	}
+	arsort($spot[0]);
+	$top_id = array_keys($spot[0]);
+
+	for ($i=0;$i<3;$i++){
+		$top_spot[$i][0] = Spot::where('id', $top_id[$i])->first()->spot_name;
+
+		$article[$i] = json_decode(shell_exec('curl -X GET https://emma.pixnet.cc/blog/articles/search\?key\="' . $top_spot[$i][0] . '"\&format\=json\&per_page\=3\&orderby\=public_at_desc\&type\=tag\&site_category_id\=28'));
+		$top_spot[$i][1] = $article[$i]->articles[0]->link;
+	}
+
+	$result = array (
+		array (
+			"spot_name" => $top_spot[0][0],
+			"article_url" => $top_spot[0][1]
+		),
+		array (
+			"spot_name" => $top_spot[1][0],
+			"article_url" => $top_spot[1][1]
+		),
+		array (
+			"spot_name" => $top_spot[2][0],
+			"article_url" => $top_spot[2][1]
+		)
+	);
+	
+	array_walk_recursive($result, function(&$value, $key){
+		if(is_string($value)){
+			$value = urlencode($value);
+		}
+	});
+	return urldecode(json_encode($result));
     }
 }
