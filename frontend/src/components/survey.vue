4fe37203826bc22e557615d64757ca69a5b8267c
xiaoswaii <b05106028@ntu.edu.tw>
2019-08-19 00:53:54 +0800

    [feat] add survey form

---
+++ frontend/src/components/survey.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <br> Q1
+      <br>
+      <div class="btn-group btn-group-vertical" data-toggle="buttons">
+        <label class="btn active">
+          <input type="radio" name='gender1' checked><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i> <span>  Male</span>
+        </label>
+        <label class="btn">
+          <input type="radio" name='gender1'><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i><span> Female</span>
+        </label>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'survey',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+@import '../assests/scss/survey.scss';
+</style>
