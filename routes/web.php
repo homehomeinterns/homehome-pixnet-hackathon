dddbb9c91ff6d09b35b043d65630d3e04223e28a
amy860702 <amy860702@gmail.com>
2019-08-18 19:29:23 +0800

    Add example_game showOptionSpots()

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::delete('/schedule_table/card/{id}', 'ScheduleController@deleteScheduleCar
 Route::get('/store_card', 'StoreController@showStoreCard');
 Route::post('/store_card', 'StoreController@addStoreCard');
 Route::delete('/store_card/{id}', 'StoreController@deleteStoreCard');
+
+Route::get('/example_game', 'ExampleController@showOptionSpots');
