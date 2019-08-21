9a91cafeb5b9ab8a10ea7ec9cd899d59228d7561
homehomeinterns <homehomeinterns@gmail.com>
2019-08-21 08:40:37 +0000

    Add scheduledcard apis

--- app/Card_schedule.php
+++ app/Card_schedule.php
@@ -3,15 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
+
 class Card_schedule extends Model
 {
-	protected $primaryKey = ['schedule_id', 'card_id'];
-	public $incrementing = false;
-
-	protected function setKeysForSaveQuery(Builder $query)
-       	{
-		return $query->where('schedule_id', $this->getAttribute('schedule_id'))
-			->where('card_id', $this->getAttribute('card_id'));
-	}
 }
