eb558dfa6fbcc3f6e2de3644984a9c77c348792c
tsuhsin <sophie0509lily@gmail.com>
2019-08-22 19:58:38 +0800

    [feat] recomend新增文章及樣式

---
+++ frontend/src/components/articleList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container col-md-6 col-lg-3">
+    <a class="block" :href="article.link" target="_blank">
+      <img :src="article.thumb">
+      <h6>{{ article.title }}</h6>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'articleList',
+  props: {
+      article: Object,
+  },
+}
+</script>
+
+<style lang="scss">
+  @import '../assets/scss/articleList.scss';
+</style>
